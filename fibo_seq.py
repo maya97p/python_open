884fb57e4b6c5c0f338194a4045162b874e74584
Maya <138455441+maya97p@users.noreply.github.com>
2023-08-05 23:39:51 +0400

    Update fibo_seq.py

--- fibo_seq.py
+++ fibo_seq.py
@@ -1,1 +1,16 @@
-
+num = int(input("Enter the number of terms: "))
+a, b = 0, 1
+count = 0
+if num <= 0:
+    print("Please enter a positive integer")
+elif num == 1:
+    print("Fibonacci sequence upto", num, ":")
+    print(a)
+else:
+    print("Fibonacci sequence:")
+    while count < num:
+        print(a)
+        c = a + b
+        a = b
+        b = c
+        count += 1
